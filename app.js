9d96f01363e9781779e52a6a1228c54d74b5b988
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-01-24 13:04:32 -0600

    Added my interests.

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const upper = myName.toUpperCase()
 console.log("Name:", upper);
 console.log('Career: Full Stack Developer / Graphic Designer');
 console.log('Description: Former bar manager taking the plunge into Web Development');
+console.log('My Interests:');
+console.log('  * Painting');
+console.log('  * Furniture Restoration');
+console.log('  * Hiking');
+console.log('  * Biking');
+
+
+function DisplayPosition(company_name,job_title,description){
+
+}
