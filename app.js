8e606f0e8f8da3da3cc768c4be556b9239e4c36c
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-01-24 12:46:57 -0600

    Added name, career, and description.

---
+++ app.js
@@ -0,0 +1,5 @@
+let myName="Maggie Rozell"
+const upper = myName.toUpperCase()
+console.log("Name:", upper);
+console.log('Career: Full Stack Developer / Graphic Designer');
+console.log('Description: Former bar manager taking the plunge into Web Development');
