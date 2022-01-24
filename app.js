24418af7a2c84b5d78af9ef69d39113180274aca
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-01-24 14:08:02 -0600

    Added my previous experience by calling the DisplayPosition function for each job.

--- app.js
+++ app.js
@@ -8,8 +8,22 @@ console.log('  * Painting');
 console.log('  * Furniture Restoration');
 console.log('  * Hiking');
 console.log('  * Biking');
+console.log('My Previous Experience:');
+let gbName='Green Bus Brewing';
+let gbTitle=' Taproom Manager';
+let gbDescription=' Oversaw all operations.';
+DisplayPosition(gbName,gbTitle,gbDescription);
+let ccName='Cork and Crust';
+let ccTitle=' MOD/Bar Manager';
+let ccDescription=' Oversaw all bar and closing operations.';
+DisplayPosition(ccName,ccTitle,ccDescription);
+let ckName='Commerce Kitchen';
+let ckTitle=' Bar Manager';
+let ckDescription=' Oversaw all bar operations and cocktail program.';
+DisplayPosition(ckName,ckTitle,ckDescription);
 
 
-function DisplayPosition(company_name,job_title,description){
 
-}
+function DisplayPosition(companyName,jobTitle,description){
+    console.log(" ", "* " + companyName + " -" + jobTitle + " -" + description);
+};
